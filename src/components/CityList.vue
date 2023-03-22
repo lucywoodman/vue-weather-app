d79420ddc27d2b1b83e90142fa1dc77bb6155f14
Lucy <lucy.woodman@wpengine.com>
2023-03-22 17:03:15 +0000

    second commit

---
+++ src/components/CityList.vue
@@ -0,0 +1,60 @@
+<template>
+    <div v-for="city in savedCities" :key="city.id">
+        <CityCard :city="city" @click="goToCityView(city)" />
+    </div>
+
+    <p v-if="savedCities.length === 0">
+        No location added. To start tracking a location, search in the field above.
+    </p>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import CityCard from "../components/CityCard.vue";
+import { useRouter } from "vue-router";
+
+const openWeatherAPIKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const savedCities = ref([]);
+const getCities = async () => {
+    if (localStorage.getItem('savedCities')) {
+        savedCities.value = JSON.parse(localStorage.getItem('savedCities'));
+
+        const requests = [];
+        savedCities.value.forEach((city) => {
+            requests.push(
+                axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=${openWeatherAPIKey}&units=metric`
+                )
+            );
+        });
+
+        const weatherData = await Promise.all(requests);
+
+        // Flicker Delay
+        await new Promise((res) => setTimeout(res, 500));
+
+        weatherData.forEach((value, index) => {
+            savedCities.value[index].weather = value.data;
+        });
+    }
+};
+
+await getCities();
+
+const router = useRouter();
+const goToCityView = (city) => {
+    router.push({
+        name: "cityView",
+        params: {
+            city: city.city,
+            state: city.state,
+        },
+        query: {
+            id: city.id,
+            lat: city.coords.lat,
+            lng: city.coords.lng,
+        },
+    });
+};
+</script>
